34ded5f45861d78c051c077bab624dcdc92e2f31
Carson <carsonkstang@gmail.com>
2018-05-27 23:27:57 -0400

    CCC_07_S3

---
+++ src/CCC/CCC_07_S3.java
@@ -0,0 +1,56 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '07 S3 - Friends
+ * Carson Tang
+ */
+public class CCC_07_S3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> [] x = new ArrayList[10000];
+        for(int i = 0; i < 10000; i++){
+            x[i] = new ArrayList<>();
+        }
+        int xc,yc;
+        for(int i  =0; i < n; i++){
+            String [] tok = br.readLine().split(" ");
+            xc = Integer.parseInt(tok[0]);
+            yc = Integer.parseInt(tok[1]);
+            x[xc].add(yc);
+        }
+        while(true){
+            String [] tok = br.readLine().split(" ");
+            xc = Integer.parseInt(tok[0]);
+            yc = Integer.parseInt(tok[1]);
+            if(xc==0 && yc==0){
+                return;
+            }
+            bfs(x,xc,yc);
+        }
+    }
+    public static void bfs(ArrayList<Integer> [] x, int start, int end){
+        LinkedList<Integer> q = new LinkedList<>();
+        int e;
+        boolean [] visited = new boolean[10000];
+        visited[start]=true;
+        q.add(start);
+        int [] dis = new int[10000];
+        while(!q.isEmpty()){
+            e=q.remove();
+            for(int i = 0; i< x[e].size(); i++){
+                if(!visited[x[e].get(i)]){
+                    visited[x[e].get(i)]=true;
+                    dis[x[e].get(i)]=dis[e]+1;
+                    q.add(x[e].get(i));
+                }
+            }
+        }
+        if(dis[end]==0){
+            System.out.println("No");
+        } else {
+            System.out.println("Yes " + (dis[end]-1));
+        }
+    }
+}
