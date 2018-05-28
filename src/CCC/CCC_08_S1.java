6eee5817007335ce32d06a13cba60416e6317a79
Carson <carsonkstang@gmail.com>
2018-05-27 23:31:15 -0400

    CCC_08_S1

---
+++ src/CCC/CCC_08_S1.java
@@ -0,0 +1,33 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '08 S1 - It's Cold Here!
+ * Carson Tang
+ */
+public class CCC_08_S1 {
+    public static void main (String [] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        Map<Integer,String> m = new HashMap<>();
+        ArrayList<Integer> y = new ArrayList<>();
+        String s;
+        int t;
+        int i =0;
+        while(true) {
+            String [] tokens = sc.readLine().split(" ");
+            s = tokens[0];
+            if(s.equals("Waterloo")){
+                i++;
+            }
+            t = Integer.parseInt(tokens[1]);
+            y.add(t);
+            m.put(t,s);
+            if(i==1){
+                Collections.sort(y);
+                Collections.reverse(y);
+                System.out.println(m.get(y.get(y.size()-1)));
+                break;
+            }
+        }
+    }
+}
