ea32cdd56a03c7976ff0b8a302a976bccf18bb69
Carson <carsonkstang@gmail.com>
2018-05-27 23:42:06 -0400

    CCC_96_S3

---
+++ src/CCC/CCC_96_S3.java
@@ -0,0 +1,52 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '96 S3 - Pattern Generator
+ * Carson Tang
+ */
+public class CCC_96_S3 {
+    public static void main (String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        String f = "";
+        String t = "";
+        String z;
+        int n,k;
+        int b;
+        for(int i = 0; i < a; i++){
+            String [] tok = br.readLine().split(" ");
+            n = Integer.parseInt(tok[0]);
+            k = Integer.parseInt(tok[1]);
+            for(int j = 0; j < k; j++){
+                f += "1";
+            }
+            for(int j = 0; j < n-k; j++){
+                f += "0";
+            }
+            z = f;
+            b = f.lastIndexOf("10");
+            System.out.println("The bit patterns are");
+            while(b!=-1){
+                System.out.println(f);
+                for(int e = 0; e < b; e++){
+                    t += f.charAt(e);
+                }
+                t += "01";
+                for(int e = f.length()-1; e >= b+2; e--){
+                    t += f.charAt(e);
+                }
+                f = t;
+                t = "";
+                b = f.lastIndexOf("10");
+            }
+            for(int j = z.length()-1; j >= 0; j--){
+                System.out.print(z.charAt(j));
+            }
+            System.out.println();
+            f="";
+            t="";
+            z="";
+        }
+    }
+}
