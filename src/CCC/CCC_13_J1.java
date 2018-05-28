d9b4f356fb77c9f02271991688625b0192cc5a6f
Carson <carsonkstang@gmail.com>
2018-05-28 19:43:54 -0400

    CCC_13_J1

---
+++ src/CCC/CCC_13_J1.java
@@ -0,0 +1,15 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '13 J1 - Next in line
+ * Carson Tang
+ */
+public class CCC_13_J1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Y = sc.nextInt();
+        int M = sc.nextInt();
+        System.out.println(M + (M-Y));
+    }
+}
