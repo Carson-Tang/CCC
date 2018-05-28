4afc32b44416d9d7153cb6dc030f377599302772
Carson <carsonkstang@gmail.com>
2018-05-27 23:09:09 -0400

    CCC_06_J1

---
+++ src/CCC/CCC_06_J1.java
@@ -0,0 +1,19 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '06 J1 - Canadian Calorie Counting
+ * Carson Tang
+ */
+public class CCC_06_J1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int [][] array = {{461,431,420,0},{100,57,70,0},{130,160,118,0},{167,266,75,0}};
+        for(int i = 0; i < 4; i++){
+            int input = sc.nextInt();
+            sum += array[i][input-1];
+        }
+        System.out.println("Your total Calorie count is " + sum + ".");
+    }
+}
