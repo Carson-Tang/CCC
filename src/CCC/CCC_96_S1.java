52d7ab192d88615601393948cd6e1162ccda44a9
Carson <carsonkstang@gmail.com>
2018-05-27 23:40:58 -0400

    CCC_96_S1

---
+++ src/CCC/CCC_96_S1.java
@@ -0,0 +1,42 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '96 S1 - Deficient, Perfect, and Abundant
+ * Carson Tang
+ */
+public class CCC_96_S1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(sc.readLine());
+        Integer[] x = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = Integer.parseInt(sc.readLine());
+        }
+        for (int i = 0; i < n; i++) {
+            if(type(x[i]).equals("abundant")){
+                System.out.println(x[i] + " is an " + type(x[i]) + " number.");
+            } else {
+                System.out.println(x[i] + " is a " + type(x[i]) + " number.");
+            }
+        }
+    }
+
+    public static String type(int n) {
+        int s = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                s += i;
+                if (s > n) {
+                    return "abundant";
+                }
+            }
+        }
+        if (s == n) {
+            return "perfect";
+        } else if (s < n) {
+            return "deficient";
+        }
+        return "";
+    }
+}
