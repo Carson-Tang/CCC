a7de9091e91060a64acd253466b6b3920c95a717
Carson <carsonkstang@gmail.com>
2018-05-27 23:50:10 -0400

    CCCHK_15_J2

---
+++ src/CCC/CCCHK_15_J2.java
@@ -0,0 +1,34 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCCHK '15 J2 - What is the minimum area?
+ * Carson Tang
+ */
+public class CCCHK_15_J2 {
+    public static void main (String [] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int [] x = new int[n];
+        int [] y = new int[n];
+        ArrayList<Integer> area = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            String [] tok =br.readLine().split(" ");
+            x[i] = Integer.parseInt(tok[0]);
+            y[i] = Integer.parseInt(tok[1]);
+        }
+        for(int i =0; i < n; i++){
+            for(int k = 0; k < n; k++){
+                if(i!=k){
+                    if(Math.abs(x[i] - x[k]) > Math.abs(y[i] - y[k])){
+                        area.add((x[i]-x[k])*(x[i]-x[k]));
+                    } else {
+                        area.add((y[i]-y[k])*(y[i]-y[k]));
+                    }
+                }
+            }
+        }
+        Collections.sort(area);
+        System.out.println(area.get(0));
+    }
+}
