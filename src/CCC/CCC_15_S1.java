036451b56cb39858c4260aa7a2bd15a00b892086
Carson <carsonkstang@gmail.com>
2018-05-28 20:39:37 -0400

    CCC_15_S1

---
+++ src/CCC/CCC_15_S1.java
@@ -0,0 +1,27 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '15 S1 - Zero That Out
+ * Carson Tang
+ */
+public class CCC_15_S1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(sc.readLine());
+        long sum =0;
+        ArrayList<Integer> x = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            int k = Integer.parseInt(sc.readLine());
+            if(k==0) {
+                x.remove(x.size()-1);
+            } else {
+                x.add(k);
+            }
+        }
+        for(int j = 0; j < x.size(); j++) {
+            sum += x.get(j);
+        }
+        System.out.println(sum);
+    }
+}
