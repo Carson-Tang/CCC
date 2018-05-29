14bb20af266651c3deaf546bb2d11da00dbaca6e
Carson <carsonkstang@gmail.com>
2018-05-28 20:37:55 -0400

    CCC_15_J1

---
+++ src/CCC/CCC_15_J1.java
@@ -0,0 +1,27 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '15 J1 - Special Day
+ * Carson Tang
+ */
+public class CCC_15_J1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        if(month == 2 && day == 18) {
+            System.out.println("Special");
+        } else if (month==2) {
+            if(day>18) {
+                System.out.println("After");
+            } else {
+                System.out.println("Before");
+            }
+        } else if (month>2) {
+            System.out.println("After");
+        } else {
+            System.out.println("Before");
+        }
+    }
+}
