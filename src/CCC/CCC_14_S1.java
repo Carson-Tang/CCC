a6a31fce1da5ea6941cdf54116dd8e5158a96895
Carson <carsonkstang@gmail.com>
2018-05-28 20:34:47 -0400

    CCC_14_S1

---
+++ src/CCC/CCC_14_S1.java
@@ -0,0 +1,37 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '14 S1 - Party Invitation
+ * Carson Tang
+ */
+public class CCC_14_S1 {
+    public static void main (String [] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(sc.readLine());
+        ArrayList<Integer>x = new ArrayList<>();
+        for(int i = 1; i <= k; i++) {
+            x.add(i);
+        }
+        int m = Integer.parseInt(sc.readLine());
+        Integer [] y = new Integer[m];
+        for(int j = 0; j<m; j++) {
+            y[j] = Integer.parseInt(sc.readLine());
+        }
+        for(int h = 0; h < m; h++) {
+            for(int f = 1; f <= x.size(); f++){
+                if(f%y[h]==0) {
+                    x.set(f-1, 0);
+                }
+            }
+            for(int z = 0; z < x.size(); z++) {
+                if (x.get(z)==0){
+                    x.remove(z);
+                }
+            }
+        }
+        for(int s = 0; s < x.size(); s++){
+            System.out.println(x.get(s));
+        }
+    }
+}
