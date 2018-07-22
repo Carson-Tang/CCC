1f503952da8c3f89bf6d21b94402e6d67669e421
Carson <carsonkstang@gmail.com>
2018-07-22 02:54:17 -0400

    CCC_07_J5

--- src/CCC/CCC_07_J5.java
+++ src/CCC/CCC_07_J5.java
@@ -6,33 +6,44 @@ import java.util.*;
  * Carson Tang
  */
 public class CCC_07_J5 {
-    public static int c =0;
-    public static void main(String [] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int g = Integer.parseInt(br.readLine());
-        int p = Integer.parseInt(br.readLine());
-        int [] parent = new int[g+1];
-        for(int i = 1; i <= g; i++) {
-            parent[i] = i;
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> h = new ArrayList<>();
+        h.add(0);
+        h.add(990);
+        h.add(1010);
+        h.add(1970);
+        h.add(2030);
+        h.add(2940);
+        h.add(3060);
+        h.add(3930);
+        h.add(4060);
+        h.add(4970);
+        h.add(5030);
+        h.add(5990);
+        h.add(6010);
+        h.add(7000);
+        for (int i = 0; i < n; i++) {
+            h.add(Integer.parseInt(br.readLine()));
         }
-        for(int i = 0; i < p; i++){
-            int dg = Integer.parseInt(br.readLine());
-            if(Find(dg,parent)==0){
-                System.out.println(c);
-                return;
+        Collections.sort(h);
+        int [] dis = new int[7001];
+        LinkedList<Integer>q = new LinkedList<>();
+        q.add(0);
+        while(!q.isEmpty()){
+            int e = q.poll();
+            for(int i = 1; i <= h.size()-1; i++){
+                if(h.get(i)>=e){
+                    if(h.get(i)-e>=a && h.get(i)-e<=b){
+                        q.add(h.get(i));
+                        dis[h.get(i)]++;
+                    }
+                }
             }
-            Union(Find(dg,parent),Find(dg,parent)-1,parent);
-            c++;
         }
-        System.out.println(c);
-    }
-    public static int Find(int v, int [] parent){
-        if(parent[v]!=v){
-            parent[v] = Find(parent[v],parent);
-        }
-        return parent[v];
-    }
-    public static void Union(int a, int b, int [] parent) {
-        parent[Find(a,parent)] = Find(b,parent);
+        System.out.println(dis[7000]);
     }
 }
