b2eb4ccffc59550148e6e96fec11274d574d2be2
Carson <carsonkstang@gmail.com>
2018-07-22 13:10:32 -0400

    CCC_97_S2

---
+++ src/CCC/CCC_97_S2.java
@@ -0,0 +1,36 @@
+package CCC;
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '97 S2 - Nasty Numbers
+ * Carson Tang
+ */
+public class CCC_97_S2 {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int d = Integer.parseInt(br.readLine());
+        for(int i = 0; i < d; i++){
+            int n = Integer.parseInt(br.readLine());
+             if (nasty(n)) System.out.println(n + " is nasty");
+             else System.out.println(n + " is not nasty");
+        }
+    }
+    public static boolean nasty(int n){
+        ArrayList<Integer>factors = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(n%i==0){
+                factors.add(i);
+            }
+        }
+        for(int i = 0; i < factors.size(); i++){
+            for(int j = 0; j < factors.size()-1; j++){
+                if(i!=j) {
+                    if (Math.abs(factors.get(factors.size() - i - 1) - factors.get(i)) == factors.get(j) + factors.get(j + 1)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
