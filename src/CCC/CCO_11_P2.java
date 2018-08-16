65d140b9503855c7d022c4e96827e43d26c69685
Carson <carsonkstang@gmail.com>
2018-08-16 04:26:37 -0400

    CCO_11_P2

--- src/CCC/CCO_11_P2.java
+++ src/CCC/CCO_11_P2.java
@@ -2,63 +2,65 @@ package CCC;
 import java.io.*;
 import java.util.*;
 /*
- * CCC '09 J4 - Signage
+ * CCO '11 P2 - Vampire Tunnels
  * Carson Tang
  */
 public class CCO_11_P2 {
+    static class edge implements Comparable<edge>{
+        int dest, dis, suntime;
+        edge(int dest, int dis, int suntime){
+            this.dest=dest;
+            this.dis=dis;
+            this.suntime=suntime;
+        }
+        public int compareTo(edge a){
+            if(this.dis>a.dis) return 1;
+            if(this.dis<a.dis) return -1;
+            return 0;
+        }
+    }
     public static void main(String [] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String [] tok = {"WELCOME","TO","CCC","GOOD","LUCK","TODAY"};
-        String s = "";
-        int wordcount=0;
-        for(int i = 0; i < 6; i++){
-            if(canAdd(n,s,tok[i])){
-                if(s.isEmpty()){
-                    s+=tok[i];
-                } else {
-                    s+='.';
-                    s+=tok[i];
-                }
-                wordcount++;
+        int s = Integer.parseInt(br.readLine());
+        String [] tok = br.readLine().split(" ");
+        int n = Integer.parseInt(tok[0]), e = Integer.parseInt(tok[1]);
+        ArrayList<edge>[]x = new ArrayList[n];
+        for(int i = 0; i < n; i++) x[i] = new ArrayList<>();
+        for(int i = 0; i < e; i++){
+            String [] tok2 = br.readLine().split(" ");
+            int a = Integer.parseInt(tok2[0]), b = Integer.parseInt(tok2[1]), d = Integer.parseInt(tok2[2]), u = Integer.parseInt(tok2[3]);
+            if(u==1){
+                x[a].add(new edge(b, d, d));
+                x[b].add(new edge(a, d, d));
             } else {
-                s = spacing(n,s,wordcount);
-                System.out.println(s);
-                wordcount=0;
-                s="";
-                i--;
+                x[a].add(new edge(b, d, u));
+                x[b].add(new edge(a, d, u));
             }
         }
-        String [] t = s.split("\\.");
-        if(t.length==1) {
-            wordcount=1;
-        } else {
-            wordcount=0;
-        }
-        if(!s.isEmpty()) {
-            s = spacing(n,s,wordcount);
-            System.out.println(s);
-        }
-    }
-    public static String spacing(int n, String s, int wordcount){
-        if(wordcount==1){
-            while(s.length()!=n){
-                s+='.';
+        PriorityQueue<edge>q = new PriorityQueue<>();
+        int [][] dis = new int[n][s+1];
+        for(int [] a : dis) Arrays.fill(a,999999);
+        q.add(new edge(0,0,s));
+        while(!q.isEmpty()){
+            edge a = q.poll();
+            int cur = a.dest;
+            for(edge b : x[cur]){
+                int len = a.dis + b.dis;
+                int light = a.suntime - b.suntime;
+                if(light<0) continue;
+                int f = b.dest;
+                if(dis[f][light]>len){
+                    dis[f][light]=len;
+                    q.add(new edge(f,len,light));
+                }
             }
-            return s;
+            if(cur==n-1) q.clear();
         }
-        for(int i = 1; i < s.length(); i++){
-            if(s.charAt(i)=='.' && s.charAt(i-1)!='.' && s.length()<n){
-                s = s.substring(0,i+1) + "." + s.substring(i+1);
-                i++;
-            }
+        int min = 999999;
+        for(int i = 0; i < s+1; i++){
+            min = Math.min(dis[n-1][i],min);
         }
-        if(s.length()==n) return s;
-        return spacing(n,s,wordcount);
-    }
-    public static boolean canAdd(int n, String s, String add){
-        if(s.isEmpty()) return true;
-        if(s.length() + add.length() + 1 <= n) return true;
-        return false;
+        if(min==999999) System.out.println(-1);
+        else System.out.println(min);
     }
 }
