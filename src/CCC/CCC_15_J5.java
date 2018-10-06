e7c3662bdbd3d13a2bfdee6c35da7ddf4aac6f8e
Carson <carsonkstang@gmail.com>
2018-10-06 00:03:36 -0400

    CCC_15_J5

---
+++ src/CCC/CCC_15_J5.java
@@ -0,0 +1,42 @@
+package CCC;
+
+import java.io.*;
+import java.util.*;
+/*
+ * CCC '15 J5 - π-day
+ * Carson Tang
+ */
+public class CCC_15_J5 {
+    static int [][][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()), k = Integer.parseInt(br.readLine());
+        dp = new int[n+1][n+1][n+1];
+        if(k==1){
+            System.out.println(1);
+        } else {
+            System.out.println(recur(1,n,k));
+        }
+        for(int i = 0; i < n/k+1; i++){
+            for(int j = 0; j < n+1; j++){
+                for(int c = 0; c < k+1; c++){
+                    System.out.print(dp[i][j][c]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static int recur (int min, int n, int k){
+        if(n==0&&k==0) return 1;
+        if(n==0||k==0) return 0;
+        if(dp[min][n][k]==0) {
+            int way = 0;
+            for (int i = min; i <= n/k; i++) {
+                way += recur(i, n - i, k - 1);
+            }
+            dp[min][n][k] = way;
+        }
+        return dp[min][n][k];
+    }
+}
