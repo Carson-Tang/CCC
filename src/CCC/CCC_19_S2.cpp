d922c9c3530c4160f4078a21f9eac4fc863fb4b5
Carson <31937503+Carson-Tang@users.noreply.github.com>
2019-03-02 21:57:46 -0500

    Create CCC_19_S2.cpp

---
+++ src/CCC/CCC_19_S2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+int t,n;
+bool isPrime(int n){
+    if(n==2) return 1;
+    if(!(n%2)) return 0;
+    for(int i = 3; i <= sqrt(n); i++){
+        if(!(n%i)) return 0;
+    } return 1;
+}
+int main(){
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cin>>t;
+    while(t--){
+        cin>>n;
+        for(int i = 2; i <= n; i++){
+            if(isPrime(i)){
+                if(isPrime(n*2-i)){
+                    cout<<i<<" "<<n*2-i<<"\n";
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
